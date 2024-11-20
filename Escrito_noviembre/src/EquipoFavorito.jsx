b9e022b0941c5d9c6ed2e79ef9a1c2a0c1f04d05
Dennis <dennis.ferreyra@anima.edu.uy>
2024-11-20 11:54:00 -0300

    pepe

--- Escrito_noviembre/src/EquipoFavorito.jsx
+++ Escrito_noviembre/src/EquipoFavorito.jsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 
 
 export const EquipoFavorito = () =>{
-   const [teams] = useState(0);
+   const [teams, setTeam] = useState({});
     useEffect(() =>{
         async function fecthUser () {
             const response = await fetch ('https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=Uruguayan%20Primera%20Division');
             const data = await response.json();
-            setUser(data.results[0])
+            setTeam(data.results[0])
             
         }
         fecthUser();
